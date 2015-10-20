3813ddf19dd955cb7112dd4482d15026a943b9e0
louisfields <luiscampos@email.arizona.edu>
2015-10-19 17:06:21 -0700

    Update MazeHolder.java

--- MazeHolder.java
+++ MazeHolder.java
@@ -1,81 +1,126 @@
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Stack;
+/*=========================================================================
+ * Author:      Luis Manuel Campos
+ * Course:      CSC 127B (Introduction to computer science II)
+ * Assignment:  Program 6 : MazeHolder.java
+ * Instructor:  L. McCann 
+ * SL:          Greg
+ * Due Date:    Ocotober 22nd 2015 
+ *
+ * Description: Polynomial implements to Quantity interface in order to carry out
+ * 				it's 12 methods that are subject to testing. Other than that, this 
+ * 			    program represents a polynomial in java code, and the methods operate 
+ * 				accoding to logial math.
+ *
+ * Operation:   Part of three java classes implemented to represent a polynomial
+ *=========================================================================*/
 
-/**
- * Developed by: Nick
- * Date of File Creation: 10/19/2015.
- * Time of File Creation: 9:29 AM
- */
+public class MazeHolder {
+	// Created a new Stack to be used
 
-public class MazeHolder{
+	// Dimension should be (2*r) +1
+	public int r, c;
 
-    private int r, c;
+	// Represents the 2D corn maze r x c;
+	public char[][] cornMaze;
 
-    private char[][] maze;
+	// 2d array of Cells
+	private Cell[][] cells;
 
-    public MazeHolder(int r, int c){
+	// Random Object used for creating the random start location
+	private Random rando = new Random();
 
-        this.r = (2*r)+1;
-        this.c = (2*c)+1;
+	public MazeHolder(int r, int c) {
+		// Assignments
+		this.r = r;
+		this.c = c;
+		// MazeHolder Dimensions
+		r = (2 * r) + 1;
+		c = (2 * c) + 1;
+		cornMaze = new char[r][c];
 
-        maze = new char[r][c];
-    }
+	}
 
-    private class Cell{
-        int x, y;
+	public class ArrayStack<Cell> {
+		private int maxR;
+		private int maxC;
+		public ArrayList<Cell> path;
+		public Cell top;
 
-        boolean isAWall = false;
-        boolean used = false;
+		public ArrayStack(int r, int c) {
+			r = maxR;
+			c = maxC;
+			path = new ArrayList<>();
+			top = path.get(path.size() - 1);
+		}
 
-        private ArrayStack a = new ArrayStack(r, c);
+		public void push(Cell empuje) {
+			path.add(empuje);
+			
+		}
+        public Cell pop() {
+			return path.get(path.size() - 1);
+		}
 
-        Cell(int x, int y){
-            this(x, y, false);
-        }
-        Cell(int x, int y, boolean isAWall){
-            this.x = x;
-            this.y = y;
-            this.isAWall = isAWall;
-        }
+		public  Cell peek() {
+			return top;
+		}
 
-        void addNeighbor(Cell other){
+		public boolean isEmpty() {
+			return path.isEmpty();
+		}
+		public boolean isFull(){
+			return top == new Cell(maxR, maxC);
+		}
 
-        }
-    }
+		
 
+	}
 
-    private class ArrayStack {
-        private int maxR;
-        private int maxC;
-        private List<Cell> path;
-        private Cell top;
+	private class Cell {
+		// X and Y coordinates
+		
+		int x, y;
+		// Array List implmented to be used to store visited cells
+		
+		ArrayStack nuevoStack = new ArrayStack(r, c);
+		
+		boolean isWall = false;
+		
+		boolean visited = false;
 
-        public ArrayStack(int r, int c){
-            maxR = r;
-            maxC = c;
-            path = new ArrayList<Cell>();
-            top = path.get(path.size()-1);
-        }
+		Cell(int x, int y) {
+		        this(x, y, true);
+		 }
+		Cell(int x, int y, boolean isWall) {
+			this.x = x;
+			this.y = y;
+			this.isWall = isWall;
+		}
+		/*void addNeighbor(Cell other) {
+	        if (!n.peek().contains(other)) { // avoid duplicates
+	        */
+	       
 
-        public void push(Cell c){
-            path.add(c);
-        }
+	}
 
-        public Cell pop(){
-            return path.get(path.size()-1);
-        }
+	
 
-        public Cell peek(){
-            return top;
-        }
+	public static void main(String[] args) {
 
-        public boolean isEmpty(){
-            return path.isEmpty();
-        }
+		Scanner keyboard = new Scanner(System.in);
 
-        public boolean isFull(){
-            return top == new Cell(maxR, maxC);
-        }
-    }
-}
+		int r = keyboard.nextInt();
 
+		int c = keyboard.nextInt();
+
+		System.out
+				.println("Please enter the dimensions of the your desired maze: "
+						+ r + c);
+
+	}
+
+	}
